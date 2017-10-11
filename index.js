b7c6622f172e1adfe61a507a8b9bc6f61a7abbdf
Drew Watson <drew@moltin.com>
2017-10-11 09:54:42 +0100

    added handlebars for view templating and folder structure for managing assets

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const MoltinGateway = require('@moltin/sdk').gateway;
+const express = require('express');
+const hbs = require('handlebars');
 
+var app = express();
 
 const Moltin = MoltinGateway({
   client_id: process.env.MOLTIN_CLIENT,
   client_secret: process.env.MOLTIN_SECRET,
 });
 
+//Register middleware
+app.use('view_engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', req, res => {
+  
+});
+
+app.listen(process.env.DEVELOPMENT_PORT);
+console.log('server started on port ' + process.env.DEVELOPMENT_PORT)
